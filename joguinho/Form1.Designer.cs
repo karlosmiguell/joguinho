2b96d41a386542b448719fc65a146d668a8d30b1
karlosmiguell <karlosmiguel11@gmail.com>
2018-08-29 17:06:14 -0300

    Initial commit

---
+++ joguinho/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace joguinho
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btJogar = new System.Windows.Forms.Button();
+            this.lbPalavra = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbChances = new System.Windows.Forms.Label();
+            this.lbLetrasUtilizadas = new System.Windows.Forms.Label();
+            this.tbLetra = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btJogar
+            // 
+            this.btJogar.Location = new System.Drawing.Point(22, 162);
+            this.btJogar.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btJogar.Name = "btJogar";
+            this.btJogar.Size = new System.Drawing.Size(333, 37);
+            this.btJogar.TabIndex = 0;
+            this.btJogar.Text = "Jogar";
+            this.btJogar.UseVisualStyleBackColor = true;
+            this.btJogar.Click += new System.EventHandler(this.btJogar_Click);
+            this.btJogar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btJogar_KeyDown);
+            // 
+            // lbPalavra
+            // 
+            this.lbPalavra.AutoSize = true;
+            this.lbPalavra.Location = new System.Drawing.Point(109, 32);
+            this.lbPalavra.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lbPalavra.Name = "lbPalavra";
+            this.lbPalavra.Size = new System.Drawing.Size(100, 21);
+            this.lbPalavra.TabIndex = 1;
+            this.lbPalavra.Text = "_ _ _ _ _";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbPalavra);
+            this.panel1.Location = new System.Drawing.Point(22, 19);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(333, 81);
+            this.panel1.TabIndex = 2;
+            // 
+            // lbChances
+            // 
+            this.lbChances.AutoSize = true;
+            this.lbChances.Location = new System.Drawing.Point(18, 213);
+            this.lbChances.Name = "lbChances";
+            this.lbChances.Size = new System.Drawing.Size(171, 21);
+            this.lbChances.TabIndex = 3;
+            this.lbChances.Text = "Quantidade de Vida: ";
+            // 
+            // lbLetrasUtilizadas
+            // 
+            this.lbLetrasUtilizadas.AutoSize = true;
+            this.lbLetrasUtilizadas.Location = new System.Drawing.Point(18, 250);
+            this.lbLetrasUtilizadas.Name = "lbLetrasUtilizadas";
+            this.lbLetrasUtilizadas.Size = new System.Drawing.Size(153, 21);
+            this.lbLetrasUtilizadas.TabIndex = 4;
+            this.lbLetrasUtilizadas.Text = "Letras Utilizadas: ";
+            // 
+            // tbLetra
+            // 
+            this.tbLetra.Location = new System.Drawing.Point(22, 126);
+            this.tbLetra.Name = "tbLetra";
+            this.tbLetra.Size = new System.Drawing.Size(333, 28);
+            this.tbLetra.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Proxima Palavra";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 333);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 34);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Reiniciar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(22, 293);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(62, 21);
+            this.lbStatus.TabIndex = 9;
+            this.lbStatus.Text = "Status:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 379);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbLetra);
+            this.Controls.Add(this.lbLetrasUtilizadas);
+            this.Controls.Add(this.lbChances);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btJogar);
+            this.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btJogar;
+        private System.Windows.Forms.Label lbPalavra;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbChances;
+        private System.Windows.Forms.Label lbLetrasUtilizadas;
+        private System.Windows.Forms.TextBox tbLetra;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lbStatus;
+    }
+}
+
---
+++ joguinho/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace joguinho
+{
+    public partial class Form1 : Form
+    {
+        //uma string para criar uma lista de palavra 
+        string[] palavras = new string[6]
+        {
+            "Abacaxi",
+            "Melão",
+            "Melancia",
+            "Manga",
+            "Uva",
+            "Abacate"
+        };
+
+
+        string palavra = "";
+
+        //Random para pegar uma letra aleatoria
+        private Random rnd = new Random();
+
+        //uma lista para pegar a letra da palavra com char
+        private List<char> letra = new List<char>();
+
+
+        // um int de chances de vida
+        int chances = 7;
+        public Form1()
+        {
+            InitializeComponent();
+            SortearPalavras();
+            lbLetrasUtilizadas.Text = "Letras Utilizadas";
+            ChancesDeVida();
+        }
+
+        private void ChancesDeVida()
+        {
+            //aqui vou atualizar toda vez 
+            // em que o jogador errar, ele perde uma vida
+            // mudando a cor 
+            lbChances.Text = "Chances: " + chances; 
+
+            if (chances >= 6)
+            {
+                lbChances.ForeColor = Color.Green;
+            }
+
+            else if (chances >=3)
+            {
+                lbChances.ForeColor = Color.Orange;
+            }
+
+            else
+            {
+                lbChances.ForeColor = Color.Red;
+            }
+        }
+
+        private void SortearPalavras()
+        {
+            //um inteiro para pegar uma palavra entre as outras 6
+            int aleatorio = rnd.Next(0, palavras.Length);
+            palavra = palavras[aleatorio];
+
+            string aparecer = "";
+
+            foreach (char letras in palavra)
+            {
+                // se tiver a letra, vai uma letra mais um espaço
+                if (letra.Contains(letras))
+                {
+                    aparecer += letra + " ";
+                }
+
+
+                // se não tiver, aparece o traço
+                else
+                {
+                    aparecer += " _ ";
+                }
+
+                lbPalavra.Text = aparecer;
+            }
+        }
+
+        private void btJogar_Click(object sender, EventArgs e)
+        {
+            if (tbLetra.Text.Length == 1)
+            {
+                char letras = Convert.ToChar(tbLetra.Text);
+                letra.Add(letras);
+                AtualizarPalavra();
+
+
+
+                tbLetra.Clear();
+                tbLetra.Focus();
+                lbStatus.Text = " ";
+                lbLetrasUtilizadas.Text += letra + "; ";
+
+                //status, verificando se tem a letra na pálavra
+                if (palavra.Contains(letras))
+                {
+                    lbStatus.Text = "Já contém esta letra na palavra!";
+                }
+
+                //se não tem a letra na palavra
+                else
+	            {
+                    chances--;
+                    ChancesDeVida();
+                    if (chances == 0)
+                    {
+                        tbLetra.Enabled = false;
+                        btJogar.Enabled = false;
+                        MessageBox.Show("Estude mais!");
+                    }
+
+                    lbStatus.Text = "Não há esta letra na palavra!";
+                }
+            }
+
+            //se não tem nenhuma letra
+            else
+            {
+                lbStatus.Text = "Digite UMA letra!";      
+            }
+            
+        }
+
+        private void AtualizarPalavra()
+        {
+            string exibirpalavra = "";
+
+
+            bool venceu = true;
+            foreach (char letrinhas in palavra)
+            {
+                if (letra.Contains(letrinhas))
+                {
+                    exibirpalavra += letrinhas + " "; 
+                }
+
+                else
+                {
+                    exibirpalavra += "_ ";
+                    venceu = false;
+                }
+
+                lbPalavra.Text = exibirpalavra;
+
+                if (venceu)
+                {
+                    tbLetra.Enabled = false;
+                    btJogar.Enabled = false;
+
+                    MessageBox.Show("você Ganhou");
+                }
+            }
+        }
+
+        private void btJogar_KeyDown(object sender, KeyEventArgs e)
+        {
+            //fui no raio, dois click "KeyDown"
+
+            // se eu apertar a tecla ENTER 
+            //ele realiza uma ação no botão jogar
+            if (e.KeyCode == Keys.Enter)
+            {
+                btJogar.PerformClick();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
